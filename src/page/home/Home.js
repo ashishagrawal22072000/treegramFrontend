c9d62b468bfa08296882c4b5d3ef95c36d8cae50
ashishagrawal22072000 <ashishmathura1234@gmail.com>
2023-01-23 18:48:53 +0530

    auth token login functionality

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import UserApi from "../../api/UserApi";
 import InnerNavbar from "../../core/innerNavbar/InnerNavbar";
+import Login from "../login/Login";
 // import UserList from "../../core/user-list/UserList";
 
 const Home = () => {
     const { auth } = useSelector(state => state.authSlice)
+    const auth_token = localStorage.getItem('auth-token');
     const [followerList, setFollowerList] = useState([])
     useEffect(() => {
         async function followers() {
@@ -17,8 +19,12 @@ const Home = () => {
     return (
 
         <>
-            {/* <UserList /> */}
-            <InnerNavbar />
+            {!auth_token ? <>
+                <Login /> </> : <>
+                <InnerNavbar />
+
+            </>
+            }
         </>
     )
 };
