bb0d064f0263ec0f055a1c8884d882ee12d476a0
ashishagrawal22072000 <ashishmathura1234@gmail.com>
2023-02-14 11:07:33 +0530

    drag and drop integrated

--- src/models/createModel/CreateModel.js
+++ src/models/createModel/CreateModel.js
@@ -1,88 +1,114 @@
 import React, { useEffect, useState } from "react";
 import DeleteIcon from "../../assets/images/img/delete.svg";
-import Multistep from 'react-multistep'
+import Multistep from "react-multistep";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Modal from "react-modal";
 import Step3 from "./Step3";
+import PreviousIcon from "../../assets/images/img/left.svg";
+import NextIcon from "../../assets/images/img/right.svg";
+import SendIcon from "../../assets/images/img/send.svg"
 const CreateModel = ({ modalIsOpen, setIsOpen, post_id }) => {
-    // const steps = [
-    //     { title: 'StepOne', component: <Step1 /> },
-    //     { title: 'StepTwo', component: <Step2 /> },
-    // ];
-    const [step, setStep] = useState(1);
-    useEffect(() => {
-        setStep(1)
-    }, [])
+  // const steps = [
+  //     { title: 'StepOne', component: <Step1 /> },
+  //     { title: 'StepTwo', component: <Step2 /> },
+  // ];
+  const [step, setStep] = useState(1);
+  useEffect(() => {
+    setStep(1);
+  }, []);
 
-    const [postData, setPostData] = useState({
-        media: [],
-        tags : [],
-        content : "",
-        location : "",
-        hashTags : [],
-        comment_status : false,
-        like_status : false,
-    })
+  const [postData, setPostData] = useState({
+    media: [],
+    tags: [],
+    content: "",
+    location: "",
+    hashTags: [],
+    comment_status: false,
+    like_status: false,
+  });
 
-    function showComponent() {
-        switch (step) {
-            case 1:
-                return <Step1 postData={postData} setPostData={setPostData} step={step} setStep={setStep} />
-            case 2:
-                return <Step2 postData={postData} setPostData={setPostData} step={step} setStep={setStep} />
-            case 3:
-                return <Step3 postData={postData} setPostData={setPostData} step={step} setStep={setStep} />
-        }
+  function showComponent() {
+    switch (step) {
+      case 1:
+        return (
+          <Step1
+            postData={postData}
+            setPostData={setPostData}
+            step={step}
+            setStep={setStep}
+          />
+        );
+      case 2:
+        return (
+          <Step2
+            postData={postData}
+            setPostData={setPostData}
+            step={step}
+            setStep={setStep}
+          />
+        );
+      case 3:
+        return (
+          <Step3
+            postData={postData}
+            setPostData={setPostData}
+            step={step}
+            setStep={setStep}
+          />
+        );
     }
-    const customStyles = {
-        content: {
-            top: "50%",
-            left: "50%",
-            right: "auto",
-            bottom: "auto",
-            marginRight: "-50%",
-            transform: "translate(-50%, -50%)",
-            height: "80%",
-            width: "80%",
-        },
-    };
+  }
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      height: "80%",
+      width: "80%",
+    },
+  };
 
-    function openModal() {
-        setIsOpen(true);
-    }
+  function openModal() {
+    setIsOpen(true);
+  }
 
-    function afterOpenModal() {
-        // references are now sync'd and can be accessed.
-        // subtitle.style.color = '#f00';
-    }
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // subtitle.style.color = '#f00';
+  }
 
-    function closeModal() {
-        setIsOpen(false);
-    }
+  function closeModal() {
+    setIsOpen(false);
+  }
 
-    return (
-        <>
-            <Modal
-                isOpen={modalIsOpen}
-                onAfterOpen={afterOpenModal}
-                onRequestClose={closeModal}
-                style={customStyles}
-                contentLabel="Example Modal"
-            >
-                {/* <Multistep activeStep={0} steps={steps} /> */}
-                <div className="create_model">
-                    <button onClick={() => setStep(step == 3 ? 1 : step + 1)}>Next</button>
-                    <button onClick={() => setStep(step == 1 ? 1 : step - 1)}>Prev</button>
-                    <form>
-                        {showComponent()}
+  return (
+    <>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        {/* <Multistep activeStep={0} steps={steps} /> */}
+        <div className="create_model">
+          <div className="model_btn">
+            <button style={{visibility : step == 1 ? "hidden" : "visible"}} onClick={() => setStep(step == 1 ? 1 : step - 1)}>
+              <img src={PreviousIcon} height={25} width={25} />
+            </button>
+            <button className="btn_icon" onClick={() => setStep(step == 3 ? 1 : step + 1)}>
+              {step == 3 ? <img src={SendIcon} height={25} width={25} /> : <img src={NextIcon} height={25} width={25} />}
+            </button>
+          </div>
 
-                    </form>
-
-                </div>
-
-            </Modal>
-        </>
-    );
+          <form>{showComponent()}</form>
+        </div>
+      </Modal>
+    </>
+  );
 };
 export default CreateModel;
--- src/models/createModel/Step1.js
+++ src/models/createModel/Step1.js
@@ -1,63 +1,47 @@
-import { useEffect, useState } from "react"
-import feedApi from "../../api/feedApi"
-import "./CreateModel.css"
-import _ from "lodash"
-import Notify from "../../core/Toast"
+import { useEffect, useState } from "react";
+import feedApi from "../../api/feedApi";
+import "./CreateModel.css";
+import _ from "lodash";
+import Notify from "../../core/Toast";
+import { FileUploader } from "react-drag-drop-files";
+import DragIcon from "../../assets/images/img/photo-and-video.svg"
 const Step1 = ({ postData, setPostData, step, setStep }) => {
-    const handleChange = async (e) => {
+  const fileTypes = ["JPEG", "PNG", "GIF", "MP4"];
 
-        let formData = new FormData();
-        _.forEach(e.target.files, file => {
-            console.log(file)
-            formData.append("file", file)
-        })
-        // console.log(e.target.files)
-        if (e.target.files.length) {
-            // console.log(Object.values(e.target.files))
-            // setPostData({ ...postData, media: Object.values(e.target.files) })
-            const res = await feedApi.uploadFile(formData)
-            if (res.success) {
-                setPostData({ ...postData, media: res.data })
-                setStep(step + 1)
-            } else {
-                Notify("error", res.data.message)
-            }
-        } else {
-            Notify("error", "Please select a file")
-        }
-        // 
-
-
-
-        // console.log(e.target.files)
-        // let picture = []
-        // for (let i = 0; i < e.target.files.length; i++) {
-        //     picture.push(e.target.files[i])
-        // }
-        // console.log(picture)
-        // // setPicture([...picture, ...e.target.files[0]])
-        // let data = new FormData();
-        // picture.forEach(image => {
-        //     console.log(image.name)
-        //     data.append('file', {
-        //         fileName: image.name
-        //     })
-        // })
-        // // picture.forEach((ele) => {
-        // //     formData.append(key, value);
-        // //   });
-        // console.log(data, "fhgfjhebfjbre")
-        // // const res = feedApi.uploadFile()
+  const handleChange = async (file) => {
+    if (file) {
+      let formData = new FormData();
+      _.forEach(file, (file) => {
+        formData.append("file", file);
+      });
+      const res = await feedApi.uploadFile(formData);
+      if (res.success) {
+        setPostData({ ...postData, media: [...postData.media, ...res.data] });
+        setStep(step + 1);
+      } else {
+        Notify("error", res.data.message);
+      }
     }
-    // console.log(picture)
-    return (
-        <>
-            <form>
+  };
+  return (
+    <>
+    <div className="step1_container">
+    <img src={DragIcon} />
+    <h2>Drag Photos or videos Here</h2>
+    <FileUploader
+        multiple={true}
+        handleChange={handleChange}
+        name="file"
+        types={fileTypes}
+      />
+    </div>
+      
+      {/* <form>
 
                 <input id="upload" type="file" accept="image/png, image/jpeg, video/mp4" multiple onChange={handleChange} />
                 <label class="button" for="upload">Select File</label>
-            </form>
-        </>
-    )
-}
-export default Step1
+            </form> */}
+    </>
+  );
+};
+export default Step1;
